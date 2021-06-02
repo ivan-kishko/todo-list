786e3513ffa8c0bb127410587c704e45f9c305f3
ivankishko <mistedphone@gmail.com>
2021-06-02 11:27:35 +0300

    Updates

---
+++ precache-manifest.a5779a988683de58db12b2169faf6913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97f4d5a11062123c0b7e32985285d11",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "41dc0095b2470008f6e0",
+    "url": "/todo-list/static/css/main.735e3df5.chunk.css"
+  },
+  {
+    "revision": "3649be2dc8b8e3193387",
+    "url": "/todo-list/static/js/2.d99582fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-list/static/js/2.d99582fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41dc0095b2470008f6e0",
+    "url": "/todo-list/static/js/main.7a4e8e3a.chunk.js"
+  },
+  {
+    "revision": "f5ee74d33c5afdf3bdbc",
+    "url": "/todo-list/static/js/runtime-main.bd857350.js"
+  }
+]);
